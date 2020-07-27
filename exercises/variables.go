bbe726b355387a11bd5c4e21b5ee60d8eb1471cb
Cesar Cruz <cesar.cruz182@gmail.com>
2020-07-26 20:39:00 -0500

    Using primitives

--- exercises/variables.go
+++ exercises/variables.go
@@ -15,4 +15,15 @@ func main() {
 	var j string
 	j = strconv.Itoa(i)
 	fmt.Printf("%v, %T\n", j, j)
+
+	// Complex numbers
+	var myComplex complex64 = 1 + 2i
+	var otherComplex complex64 = 2 - 8i
+	complexMulti := myComplex * otherComplex
+	fmt.Printf("Complex operation: %v %v\n", real(complexMulti), imag(complexMulti))
+
+	// Text - rune
+	var r rune = 'a'
+	fmt.Printf("%v %T\n", r, r)
+
 }
